2dcf8736874e3b4f3d74ad474bcc911c26aa51ed
AmrHassan95 <amr.hassan.2@deliveryhero.com>
2025-04-21 20:59:08 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Amr Hassan on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
