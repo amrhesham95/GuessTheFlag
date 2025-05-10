3a25c5c6efd08fb60227179823b0cfde741d00d4
AmrHassan95 <amr.hassan.2@deliveryhero.com>
2025-05-10 11:51:47 +0300

    add buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland",  "Italy", "Spain", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                .foregroundStyle(.white)
+                
+                ForEach(0..<3) { index in
+                    Button {
+                        //
+                    } label: {
+                        Image(countries[index])
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
